e6aab307ff5fcf63bfaaa3e8b3528285f689277b
Saqlain Dawood <saqlaindawood123@gmail.com>
2026-02-26 00:28:59 -0800

    create classes

--- src/Pages/AdminDashboard/Classes/CreateClass.jsx
+++ src/Pages/AdminDashboard/Classes/CreateClass.jsx
@@ -7,6 +7,9 @@ import {
   MDBIcon,
 } from "mdb-react-ui-kit";
 const CreateClass = () => {
+  const handleSubmit = async(e)=>{
+    
+  }
   return (
      <>
      <MDBContainer className="py-4">
@@ -19,16 +22,95 @@ const CreateClass = () => {
             </h3>
             <MDBBtn color='secondary'
              className="me-1">
-              [Stpet 1-3]
+              [Step 1-3]
             </MDBBtn>
         </div>
         <div className="card mb-4 mt-4">
            <div className="card-header bg-primary text-white">
               <h5 className="mb-0">
-                     <i className="fas fa-user me-2"></i>
-                     Class Form..........
+                     <i className="fas fa-user me-2"></i>                
                   </h5>
            </div>
+           <div className="card-body">
+                  <div className="row g-3">
+                    <div className="col-md-6">
+                      <label className="form-label">Coordinator ID</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="coordId"
+                        value={formData.coordId}
+                        onChange={handleInputChange}
+                        required
+                      />
+                    </div>
+                    <div className="col-md-6">
+                      <label className="form-label">Full Name *</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleInputChange}
+                        required
+                      />
+                    </div>
+                    <div className="col-md-6">
+                      <label className="form-label">Email Address *</label>
+                      <input
+                        type="email"
+                        className="form-control"
+                        name="user.email"
+                        value={formData.user?.email}
+                        onChange={handleInputChange}
+                        required
+                      />
+                    </div>
+                    <div className="col-md-6">
+                      <label className="form-label">Phone Number *</label>
+                      <input
+                        type="tel"
+                        className="form-control"
+                        name="phone"
+                        value={formData.phone}
+                        onChange={handleInputChange}
+                        required
+                      />
+                    </div>
+                    <div className="col-md-6">
+                      <label className="form-label">CNIC *</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="cnic"
+                        value={formData.cnic}
+                        onChange={handleInputChange}
+                        maxLength="13"
+                        required
+                      />
+                    </div>
+                    <div className="col-md-6">
+                      <label className="form-label">Date of Birth</label>
+                      <input
+                        type="date"
+                        className="form-control"
+                        name="DOB"
+                        value={formData.DOB}
+                        onChange={handleInputChange}
+                      />
+                    </div>
+                    <div className="col-12">
+                      <label className="form-label">Address</label>
+                      <textarea
+                        className="form-control"
+                        name="address"
+                        value={formData.address}
+                        onChange={handleInputChange}
+                        rows="2"
+                      />
+                    </div>
+                  </div>
+                </div>
         </div>
         </MDBCardBody>
 
