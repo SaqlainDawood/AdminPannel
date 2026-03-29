1d1f07853e4a93b91883cb22356d1a7102edb8c8
Saqlain Dawood <saqlaindawood123@gmail.com>
2026-03-28 22:22:18 -0700

    Updated css

--- src/Pages/AdminDashboard/Attendence/Overview.jsx
+++ src/Pages/AdminDashboard/Attendence/Overview.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import AdminAPI from '../../../api';
-import './AttencanceOverview.css'
+import './AttendanceOverview.css'
 
 const AttendanceOverview = () => {
   const navigate = useNavigate();
